b2ea06596ee683e8ebfce09ce24c0090ac5cd801
Sean Dalrymple <sdalrymple@colgate.edu>
2012-10-17 16:49:54 -0400

    updates

--- lab05/threadshare.c
+++ lab05/threadshare.c
@@ -6,6 +6,11 @@
 /**
  * worker thread entry point
  */
+
+
+//"global, yuck" -- Prof. Sommers
+pthread_mutex_t mutex;
+ 
 void *worker(void *varg)
 {
     long *counter = (long *)varg;
@@ -13,7 +18,9 @@ void *worker(void *varg)
     printf("thread 0x%0lx starting\n", (long)pthread_self());
     int i = 0;
     for (; i < 100000; i += 1) {
+	pthread_mutex_lock(&mutex);
         *counter += 1;
+	pthread_mutex_unlock(&mutex);
     }
     printf("thread 0x%0lx ending with counter %ld\n", (long)pthread_self(), *counter);
 
@@ -25,6 +32,7 @@ int
 main(int argc, char **argv)
 {
     pthread_t thread_a, thread_b;
+    pthread_mutex_init(&mutex,NULL);
 
     // the shared variable - allocated on the heap
     long *counter = malloc(sizeof(long));
@@ -39,5 +47,6 @@ main(int argc, char **argv)
     printf("end counter value (main thread): %ld\n", *counter);
 
     free(counter);
+    pthread_mutex_destroy(&mutex);
     return 0;
 }
